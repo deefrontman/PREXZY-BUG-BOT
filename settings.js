b3286f2943e4ae558801aceb0dc9ac1f93a1e9c0
deefrontman <frontman643@gmail.com>
2025-01-03 23:00:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2349152619889'  //creator number
+global.ownername = '50_50🥷👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
